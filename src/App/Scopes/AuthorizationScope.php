09185e75e4d8525a49db6462150b340c2b1345d7
Marko Dupor <marko.dupor@asseco-see.hr>
2020-07-18 22:42:48 +0200

    first commit

---
+++ src/App/Scopes/AuthorizationScope.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace Voice\SearchQueryBuilder\App\Scopes;
+
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Scope;
+use Voice\JsonQueryBuilder\JsonQuery;
+
+class AuthorizationScope implements Scope
+{
+    /**
+     * Apply the scope to a given Eloquent query builder.
+     *
+     * @param \Illuminate\Database\Eloquent\Builder $builder
+     * @param \Illuminate\Database\Eloquent\Model $model
+     * @return void
+     */
+    public function apply(Builder $builder, Model $model)
+    {
+        $input = $this->getAuthValues();
+
+        $jsonQuery = new JsonQuery($builder, $input);
+        $jsonQuery->search();
+
+        //$builder->where('age', '>', 200);
+    }
+
+    protected function getAuthValues(): array
+    {
+        // Dohvati iz baze, storaj u redis.
+
+        return [];
+    }
+}
---
+++ src/JsonAuthorizationServiceProvider.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace Voice\SearchQueryBuilder;
+
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Support\ServiceProvider;
+use Voice\JsonQueryBuilder\JsonQuery;
+
+class JsonAuthorizationServiceProvider extends ServiceProvider
+{
+    /**
+     * Bootstrap the application services.
+     */
+    public function boot()
+    {
+        Builder::macro('search', function (array $input) {
+            /**
+             * @var $this Builder
+             */
+            $jsonQuery = new JsonQuery($this, $input);
+            $jsonQuery->search();
+            //dd($this->dump());
+            return $this;
+        });
+    }
+
+    /**
+     * Register the application services.
+     */
+    public function register()
+    {
+
+    }
+
+}
