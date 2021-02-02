9802f50d6891ad8d995992c93735611a46865172
Marko Dupor <Norgul@users.noreply.github.com>
2021-02-02 14:44:53 +0100

    Apply fixes from StyleCI (#12)

--- tests/Unit/App/Models/AuthorizationRuleTest.php
+++ tests/Unit/App/Models/AuthorizationRuleTest.php
@@ -15,7 +15,7 @@ class AuthorizationRuleTest extends TestCase
         parent::setUp();
 
         config(['asseco-authorization.models_path' => [
-            __DIR__ . '/../../../' => 'Asseco\\JsonAuthorization\\Tests\\'
+            __DIR__ . '/../../../' => 'Asseco\\JsonAuthorization\\Tests\\',
         ]]);
     }
 
