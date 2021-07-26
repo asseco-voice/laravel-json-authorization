0cd1b4899ff1e331b967fd38f97ebad3815c34d8
Marko Dupor <marko.dupor@asseco-see.hr>
2021-07-26 14:48:49 +0200

    Fix

--- src/Authorization/AbsoluteRights.php
+++ src/Authorization/AbsoluteRights.php
@@ -16,10 +16,10 @@ class AbsoluteRights
 
         foreach ($absoluteRights as $authorizableSetType => $authorizableSetValues) {
 
-            /** @var AuthorizableSetType $authorizableSetType */
-            $authorizableSetType = config('asseco-authorization.authorizable_set_type_model');
+            /** @var AuthorizableSetType $authorizableSetTypeModel */
+            $authorizableSetTypeModel = config('asseco-authorization.authorizable_set_type_model');
 
-            $resolvedSetType = $authorizableSetType::cached()->where('name', $authorizableSetType)->first();
+            $resolvedSetType = $authorizableSetTypeModel::cached()->where('name', $authorizableSetType)->first();
             $setTypeId = Arr::get($resolvedSetType, 'id');
 
             $userRules = $authorizationRules->where('authorizable_set_type_id', $setTypeId);
